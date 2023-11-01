0935c9773482ef30349df744a7d0b4610fe82da4
Christopher <chrisjwkim97@gmail.com>
2023-11-01 11:33:44 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number);
+        number--;
+    }
+}
